122a68556d9b4756e9d52c2ea087c0af2131d285
Resheil Agarwal <resheil@resheil.com>
2022-10-10 14:58:26 +0530

    Adding Header and footer

---
+++ header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Chokshi & Chokshi LLP</title>
+    <!-- web fonts -->
+    <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900&display=swap" rel="stylesheet">
+    <!-- //web fonts -->
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+  </head>
+  <body>
+<div class="w3l-bootstrap-header fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-light p-2">
+    <div class="container">
+     <!--- <a class="navbar-brand" href="index.html"><span class="fa fa-diamond"></span>Market</a> -->
+      
+    <a class="navbar-brand" href="#index.php">
+        <img src="chokshi.png" alt="Your logo" title="Your logo" style="height:55px;" />
+    </a> 
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+        </ul>
+        <!---
+		<div class="form-inline">
+          <a href="#login.html" class="login mr-4">Log in</a>
+            <a href="#signup.html" class="btn btn-primary btn-theme">Create Free Account</a>
+        </div>
+		--->
+      </div>
+    </div>
+  </nav>
+</div>
